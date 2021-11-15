f2215818de1a2f0674472b1459685d72c8c07959
laken11 <olaniteolalekan@gmail.com>
2021-11-15 11:46:46 +0100

    Mazstar don arrive

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mazstar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mazstar");
+        }
+    }
+}
